09e6f10bdf89945852fea05c335b14c0d57b8301
Farai <fabvu@Farais-MacBook-Pro.local>
2023-06-06 00:34:52 -0300

    feat: add animate text component

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,10 @@
 import Layout from "@/components/Layout";
 import Head from "next/head";
 import Image from "next/image";
+import Link from "next/link";
 import profilePic from "../../public/images/profile/developer-pic-1.png";
+import AnimatedText from "@/components/AnimatedText";
+import { LinkArrow } from "@/components/Icons";
 
 export default function Home() {
   return (
@@ -16,18 +19,40 @@ export default function Home() {
             <div className="w-1/2">
               <Image
                 src={profilePic}
-                alt="AI Generated Farai"
+                alt="AI Generated Profile Picture"
                 className="w-full h-auto"
               />
             </div>
-            <div>
-              <h1>Turning Vision Into Reality With Code And Design. </h1>
-              <p>
+            <div className="w-1/2 flex flex-col items-center self-center">
+              <AnimatedText
+                text="Turning Vision Into Reality With Code And Design."
+                className="!text-6xl !text-left"
+              />
+              <p className="my-4 text-base font-medium">
                 As a skilled front-end engineer, I am dedicated to turning ideas
                 into innovative web applications. Explore my latest projects and
                 articles, showcasing my expertise in React.js and web
                 development.
               </p>
+              <div className="flex items-center self-start mt-2">
+                <Link
+                  href="/resume.pdf"
+                  target={"_blank"}
+                  className="flex items-center bg-dark text-light p-2.5 px-6 rounded-lg text-lg font-semibold hover:bg-light hover:text-dark
+                  border-2 border-solid border-transparent hover:border-dark"
+                  download={true}
+                >
+                  Resumé <LinkArrow className={"w-6 ml-1"} />
+                  {/* switch icon */}
+                </Link>
+                <Link
+                  href="mailto:fabvu8@gmail.com"
+                  target={"_blank"}
+                  className="ml-4 textlg font-medium capitalize text-dark underline"
+                >
+                  Contact
+                </Link>
+              </div>
             </div>
           </div>
         </Layout>
