ada4ca648928c7942f39ddb104c97ad5af80e27c
Farai <fabvu@Farais-MacBook-Pro.local>
2023-06-12 23:06:30 -0300

    chore: optimize next.js images

--- src/pages/about.js
+++ src/pages/about.js
@@ -70,6 +70,8 @@ function about() {
                 src={profilePic}
                 alt="Farai profile picture"
                 className="w-full h-auto rounded-xl"
+                priority
+                sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
               />
             </div>
             <div className="col-span-2 flex flex-col items-end justify-between">
